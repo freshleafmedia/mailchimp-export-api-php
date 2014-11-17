c04cabc4faba970d46b64dd368bf9e8fa803ca11
Stephan Schulze <sschulze@silversurfer7.de>
2014-11-17 16:57:22 +0100

    added guzzle as connection client

--- src/MailchimpExport.php
+++ src/MailchimpExport.php
@@ -1,26 +1,57 @@
 <?php
 
+use Guzzle\Http\Client;
+use Guzzle\Stream\PhpStreamRequestFactory;
+use Guzzle\Stream\StreamInterface;
+
 require_once 'MailchimpExportDataCallbackInterface.php';
 require_once 'MailchimpExport/Ecomm.php';
 require_once 'MailchimpExport/Lists.php';
 require_once 'MailchimpExport/CampaignSubscriberActivity.php';
 require_once 'MailchimpExport/Exceptions.php';
 
-class MailchimpExport {
-    
+class MailchimpExport
+{
+
+    const REQUEST_TIMEOUT = 600;
     public $apikey;
     public $ch;
-    public $root  = 'https://api.mailchimp.com/export/1.0';
+    public $root = 'https://api.mailchimp.com/export/1.0';
     public $debug = false;
-
-    /** @var \MailchimpExport_Ecomm  */
+    /** @var \MailchimpExport_Ecomm */
     public $ecomm;
-    /** @var \MailchimpExport_Lists  */
+    /** @var \MailchimpExport_Lists */
     public $lists;
-    /** @var \MailchimpExport_CampaignSubscriberActivity  */
+    /** @var \MailchimpExport_CampaignSubscriberActivity */
     public $campainSubscriberActivity;
+    /** @var  Client $client */
+    private $client;
 
-    public function __construct($apikey=null, $opts=array()) {
+    /**
+     * @param null $apikey
+     * @param array $opts
+     */
+    public function __construct($apikey = null, $opts = array())
+    {
+
+        $this->apikey = $this->initializeApikey($apikey);
+        $this->root = $this->initializeRequestRoot($this->root, $this->apikey);
+        $this->client = $this->initializeClient($this->root, $opts);
+
+        // initialize provided apis
+        $this->ecomm = new MailchimpExport_Ecomm($this);
+        $this->lists = new MailchimpExport_Lists($this);
+        $this->campainSubscriberActivity = new MailchimpExport_CampaignSubscriberActivity($this);
+    }
+
+    /**
+     * @param $apikey
+     * @return bool|string
+     * @throws Mailchimp_Error
+     */
+    protected function initializeApikey($apikey)
+    {
+
         if (!$apikey) {
             $apikey = getenv('MAILCHIMP_APIKEY');
         }
@@ -28,167 +59,178 @@ class MailchimpExport {
         if (!$apikey) {
             $apikey = $this->readConfigs();
         }
-
         if (!$apikey) {
             throw new Mailchimp_Error('You must provide a MailChimp API key');
         }
+        return $apikey;
+    }
 
-        $this->apikey = $apikey;
-        $dc           = "us1";
+    /**
+     * @param $root
+     * @param $apiKey
+     * @return mixed|string
+     */
+    protected function initializeRequestRoot($root, $apiKey) {
+        $dc = "us1";
 
-        if (strstr($this->apikey, "-")){
-            list($key, $dc) = explode("-", $this->apikey, 2);
+        if (strstr($apiKey, "-")) {
+            list($key, $dc) = explode("-", $apiKey, 2);
             if (!$dc) {
                 $dc = "us1";
             }
         }
 
-        $this->root = str_replace('https://api', 'https://' . $dc . '.api', $this->root);
-        $this->root = rtrim($this->root, '/') . '/';
+        $root = str_replace('https://api', 'https://' . $dc . '.api', $root);
+        $root = rtrim($root, '/') . '/';
+        return $root;
+    }
 
-        if (!isset($opts['timeout']) || !is_int($opts['timeout'])){
-            $opts['timeout'] = 600;
+    /**
+     * @param $root
+     * @param array $opts
+     * @return Client
+     */
+    protected function initializeClient($root, array $opts) {
+        if (!isset($opts['timeout']) || !is_int($opts['timeout'])) {
+            $opts['timeout'] = self::REQUEST_TIMEOUT;
         }
-        if (isset($opts['debug'])){
+        if (isset($opts['debug'])) {
             $this->debug = true;
         }
 
+        $clientOptions = array(
+            'curl.options' => array(
+                'CURLOPT_POST' => true,
+                'CURLOPT_CONNECTTIMEOUT' => 30,
+                'CURLOPT_TIMEOUT' => $opts['timeout'],
+            ),
+        );
 
-        $this->ch = curl_init();
+        $client = new Client($this->root, $clientOptions);
+        $client->setUserAgent('Mailchimp-PHP/1.0');
 
+        $client->setDefaultOption('allow_redirects', false);
         if (isset($opts['CURLOPT_FOLLOWLOCATION']) && $opts['CURLOPT_FOLLOWLOCATION'] === true) {
-            curl_setopt($this->ch, CURLOPT_FOLLOWLOCATION, true);    
+            $client->setDefaultOption('allow_redirects', true);
         }
-
-        curl_setopt($this->ch, CURLOPT_USERAGENT, 'MailChimp-PHP/2.0.6');
-        curl_setopt($this->ch, CURLOPT_POST, true);
-        curl_setopt($this->ch, CURLOPT_HEADER, false);
-        curl_setopt($this->ch, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($this->ch, CURLOPT_CONNECTTIMEOUT, 30);
-        curl_setopt($this->ch, CURLOPT_TIMEOUT, $opts['timeout']);
-
-        $this->ecomm = new MailchimpExport_Ecomm($this);
-        $this->lists = new MailchimpExport_Lists($this);
-        $this->campainSubscriberActivity = new MailchimpExport_CampaignSubscriberActivity($this);
+        return $client;
     }
 
-    public function __destruct() {
-        if(is_resource($this->ch)) {
-            curl_close($this->ch);
-        }
-    }
-
-    public function call($url, $params, MailchimpExport_DataCallbackInterface $dataCallable) {
-
-        $params['apikey'] = $this->apikey;
-        
-        $ch     = $this->ch;
-
-        curl_setopt($ch, CURLOPT_URL, $this->root . $url);
-        curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($params));
-        curl_setopt($ch, CURLOPT_VERBOSE, $this->debug);
-
-        $headings = null;
-        $hasHeadingRow = $dataCallable->hasHeadingRow();
-
-        curl_setopt($this->ch, CURLOPT_WRITEFUNCTION, function($ch, $data) use ($headings, $hasHeadingRow, $dataCallable) {
-                $dataLength = strlen($data);
-                if ($dataLength == 0) {
-                    return $dataLength;
-                }
-                $result = explode("\n", $data);
-                $data = null;
-
-                do {
-                    $currentRow = array_shift($result);
-
-                    if (empty($currentRow)) {
-                        continue;
-                    }
-
-
-                    $currentDataSet = json_decode($currentRow, true);
-                    $currentRow = null;
-
-                    // check for errors
-                    if (isset($currentDataSet['error']) && isset($currentDataSet['code'])) {
-                        throw new \MailchimpExport_Exception($currentDataSet['error'], $currentDataSet['code']);
-                    }
-
-                    if ($headings === null && $hasHeadingRow) {
-                        $headings = $currentDataSet;
-                        continue;
-                    }
-
-                    // if we have headings => combine them with the dataset
-                    if ($headings !== null) {
-                        $returnValue = array_combine($headings, $currentDataSet);
-                    }
-                    else {
-                        $returnValue = $currentDataSet;
-                    }
-
-                    $dataCallable->addDataSet($returnValue);
-                    $returnValue = null;
-                    $currentDataSet = null;
-                }
-                while (!empty($result));
-                $result = null;
-                return $dataLength;
-            });
-
-
-        $start = microtime(true);
-        $this->log('Call to ' . $this->root . $url . ': ' . json_encode($params));
-        $curl_buffer = null;
-        if($this->debug) {
-            $curl_buffer = fopen('php://memory', 'w+');
-            curl_setopt($ch, CURLOPT_STDERR, $curl_buffer);
-        }
-
-        curl_exec($ch);
-
-        if($this->debug) {
-            rewind($curl_buffer);
-            $this->log(stream_get_contents($curl_buffer));
-            fclose($curl_buffer);
-        }
-
-        $time = microtime(true) - $start;
-        $this->log('Completed in ' . number_format($time * 1000, 2) . 'ms');
-
-        if(curl_error($ch)) {
-            throw new Mailchimp_HttpError("API call to $url failed: " . curl_error($ch));
-        }
-    }
-
-    public function readConfigs() {
+    /**
+     * @return bool|string
+     */
+    public function readConfigs()
+    {
         $paths = array('~/.mailchimp.key', '/etc/mailchimp.key');
-        foreach($paths as $path) {
-            if(file_exists($path)) {
+        foreach ($paths as $path) {
+            if (file_exists($path)) {
                 $apikey = trim(file_get_contents($path));
                 if ($apikey) {
                     return $apikey;
                 }
             }
         }
+
         return false;
     }
 
-    public function castError($result) {
-        if ($result['status'] !== 'error' || !$result['name']) {
-            throw new MailchimpExport_Error('We received an unexpected error: ' . json_encode($result));
-        }
-
-        $class = (isset(self::$error_map[$result['name']])) ? self::$error_map[$result['name']] : 'MailchimpExport_Error';
-        return new $class($result['error'], $result['code']);
+    /**
+     * @param $url
+     * @param $params
+     * @param MailchimpExport_DataCallbackInterface $dataCallable
+     */
+    public function call($url, $params, MailchimpExport_DataCallbackInterface $dataCallable)
+    {
+        $params['apikey'] = $this->apikey;
+        $params = $this->filterParameter($params);
+        $stream = $this->createStream($url, $params);
+        $this->readFromStream($stream, $dataCallable);
     }
 
-    public function log($msg) {
+    /**
+     * @param array $params
+     * @return array
+     */
+    protected function filterParameter(array $params)
+    {
+        // filter null values
+        foreach ($params as $key => $value) {
+            if ($value === null) {
+                unset($params[$key]);
+            }
+        }
+
+        return $params;
+    }
+
+    /**
+     * @param $url
+     * @param array $params
+     * @return \Guzzle\Stream\StreamInterface
+     */
+    protected function createStream($url, array $params)
+    {
+        $request = $this->client->post($url, null, $params);
+
+        $factory = new PhpStreamRequestFactory();
+
+        return $factory->fromRequest($request);
+    }
+
+    /**
+     * @param StreamInterface $stream
+     * @param MailchimpExport_DataCallbackInterface $dataCallable
+     * @throws MailchimpExport_Exception
+     */
+    protected function readFromStream(StreamInterface $stream, MailchimpExport_DataCallbackInterface $dataCallable)
+    {
+        $headings = array();
+        $first = true;
+        $hasHeadingRow = $dataCallable->hasHeadingRow();
+
+        // Read until the stream is closed
+        while (!$stream->feof()) {
+            // Read a line from the stream
+            $currentRow = $stream->readLine();
+            $currentDataSet = json_decode($currentRow, true);
+
+            if (!$currentDataSet) {
+                throw new \MailchimpExport_Exception('invalid response: ' . $currentRow);
+            }
+
+            $currentRow = null;
+
+            // check for errors
+            if (isset($currentDataSet['error']) && isset($currentDataSet['code'])) {
+                throw new \MailchimpExport_Exception($currentDataSet['error'], $currentDataSet['code']);
+            }
+
+            if ($first && $hasHeadingRow) {
+                $headings = $currentDataSet;
+                $first = false;
+                continue;
+            }
+
+            // if we have headings => combine them with the dataset
+            if ($headings !== null) {
+                $returnValue = array_combine($headings, $currentDataSet);
+            } else {
+                $returnValue = $currentDataSet;
+            }
+
+            $dataCallable->addDataSet($returnValue);
+            $returnValue = null;
+            $currentDataSet = null;
+        }
+    }
+
+    public function log($msg)
+    {
         if ($this->debug) {
             error_log($msg);
         }
     }
+
 }
 
 
